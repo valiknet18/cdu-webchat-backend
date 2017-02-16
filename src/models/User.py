d7a9d0a748042297b3a8dfe32b6dc0998458cc8a
valik-pc <valik.v1per@gmail.com>
2017-02-16 19:52:32 +0200

    Initial commit

---
+++ src/models/User.py
@@ -0,0 +1,8 @@
+from src.application import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
