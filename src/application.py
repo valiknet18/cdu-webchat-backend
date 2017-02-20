1ec6ae60dbfe46cf26d017d5d033370e1ea63ec7
valik-pc <valik.v1per@gmail.com>
2017-02-21 00:09:04 +0200

    Implement serialization model

--- src/application.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123123@localhost/cdu_webchat'
-
-db = SQLAlchemy(app)
--- src/models/User.py
+++
@@ -1,8 +0,0 @@
-from src.application import db
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
